3b46de86e434080d3b91c2863f1aeaa60454fb88
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2024-05-01 00:54:34 -0700

    Update

--- AccountingLedger/src/main/java/org/example/Customer.java
+++ AccountingLedger/src/main/java/org/example/Customer.java
@@ -4,7 +4,6 @@ public class Customer {
      String  name;
     double balance;
 
-
     public Customer(String name,  double balance){
         this.name = name;
         this.balance = balance;
---
+++ AccountingLedger/src/main/java/org/example/Deposit.java
@@ -0,0 +1,15 @@
+package org.example;
+
+abstract class Transactions {
+
+}
+
+public class Deposit extends Transactions {
+
+}
+
+
+
+
+
+
